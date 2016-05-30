60f6b7d1db0884da03f2ab70835bb78b1036aeca
Chris Freeman <cfreeman.code@gmail.com>
2016-05-30 11:36:37 -0500

    set up basic password hashing and confirmation on User model, plus user tests

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,15 @@ config :plug, :mimes, %{
   "application/vnd.api+json" => ["json-api"]
 }
 
+config :guardian, Guardian,
+  allowed_algos: ["HS512"],
+  verify_module: Guardian.JWT,
+  issuer: "Peepchat",
+  ttl: { 30, :days },
+  verify_issuer: true,
+  secret_key: System.get_env("GUARDIAN_SECRET") || "5iD8hQoU+dTATKSHtvjljYqJ1c7sWx9wMH6zvuhuJOSHxAjumPWRNZV35Z6v0lCB",
+  serializer: Peepchat.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
