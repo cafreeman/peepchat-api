a9e3a3252774b00178f25b1af1c2dc7cb640e7b7
Chris Freeman <cfreeman.code@gmail.com>
2016-05-30 13:01:53 -0500

    section 2.1: update error handling, make JSON-API compliant, and /api/user/current route

--- test/controllers/registration_controller_test.exs
+++ test/controllers/registration_controller_test.exs
@@ -6,7 +6,7 @@ defmodule Peepchat.RegistrationControllerTest do
   @valid_attrs %{
     email: "chris@example.com",
     password: "abcde12345",
-    password_confirmation: "abcde12345"
+    "password-confirmation": "abcde12345"
   }
 
   @invalid_attrs %{}
@@ -16,15 +16,14 @@ defmodule Peepchat.RegistrationControllerTest do
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, registration_path(conn, :create), %{data: %{type: "user", attributes: @valid_attrs}}
+    conn = post conn, registration_path(conn, :create), %{data: %{type: "users", attributes: @valid_attrs}}
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(User, %{email: @valid_attrs[:email]})
   end
 
   test "does not create resource and renders errors when data is in valid", %{conn: conn} do
     assert_error_sent 400, fn ->
-      conn = post conn, registration_path(conn, :create), %{data: %{type: "user", attributes: @invalid_attrs}}
+      post conn, registration_path(conn, :create), %{data: %{type: "user", attributes: @invalid_attrs}}
     end
   end
-
 end
